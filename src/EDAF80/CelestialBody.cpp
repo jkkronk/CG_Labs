df0f9b207d3a0d771be4828f8c3dd6ec4a9f0a39
Jonatan Kronander <elt15jkr@student.lth.se>
2019-09-09 18:07:22 +0200

    lab0 init

---
+++ src/EDAF80/CelestialBody.cpp
@@ -0,0 +1,17 @@
+#include "CelestialBody.hpp"
+#include "../core/helpers.hpp"
+
+
+edaf80::CelestialBody(bonobo::mesh_data const& shape, GLuint const* program, GLuint diffuse_texture_id) :
+{
+	//! Implement
+	
+}
+
+bonobo::mesh_data
+edaf80::render(float ellapsed_time, glm::mat4 const& view_projection, glm::mat4 const& parent_transform) :
+{
+	//! Implement 
+
+
+}
